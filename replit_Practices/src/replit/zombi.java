3b12f3243031ba22722a4b56a836784830a5f01b
Nurulla <numuminov@gmail.com>
2020-08-05 21:53:41 -0400

    add NewClass2.java

--- replit_Practices/src/replit/zombi.java
+++ replit_Practices/src/replit/zombi.java
@@ -1,14 +1,45 @@
 package replit;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class zombi {
     public static void main(String[] args) {
-        int count=0;
-        int i=100;
+        Scanner input = new Scanner(System.in);
+        int[] inhabitants = new int[8];
+        for(int i=0; i<inhabitants.length; i++) {
+            inhabitants[i] = input.nextInt();
+        }
 
-        while (i>0){
+        //TODO. Write you code below this line.
 
-            System.out.println("Day"+(count=count+1) +"["+i+"]");
-            i=i/2;
+
+
+    for (int i = 0; i <= inhabitants.length - 2; i++) {
+        if (inhabitants[i] == 0) {
+            inhabitants[i + 1] = inhabitants[i + 1] / 2;
         }
     }
+    for (int i = inhabitants.length - 1; i >= 1; i--) {
+        if (inhabitants[i] == 0) {
+            inhabitants[i - 1] = inhabitants[i - 1] / 2;
+        }
+    }
+    System.out.println(Arrays.toString(inhabitants));
+
+
 }
+
+            }
+
+
+
+
+
+
+
+
+
+
+
