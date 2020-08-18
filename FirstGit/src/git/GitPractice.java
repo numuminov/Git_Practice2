c7011243ee749833fb048136bbe868e5ceb230b9
Nurulla <numuminov@gmail.com>
2020-08-18 18:02:24 -0400

    Commit 1st

--- FirstGit/src/git/GitPractice.java
+++ FirstGit/src/git/GitPractice.java
@@ -1,4 +1,13 @@
 package git;
 
 public class GitPractice {
+
+    public static void main(String[] args) {
+        String str1 = "ffr";
+        String str2 = "ggt";
+        String str3 = str1 + str2;
+
+        System.out.print(str3);
+// added changes to learn how to pull from github 
+    }
 }
